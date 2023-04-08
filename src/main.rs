1ca80f1a1b6f664c818b1aef86b752da31ecaa2e
Doct0rLekter <marikwinters@gmail.com>
2023-04-08 13:50:09 -0400

    Refactor logic into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 // Set clippy to pedantic
 #![warn(clippy::pedantic)]
 
-use std::env;
-use std::error::Error;
-use std::fs;
-use std::process;
+use std::env; // For env::args()
+use std::process; // For process::exit()
+
+use minigrep::{self, Config};
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -16,33 +17,9 @@ fn main() {
     println!("Searching for: {}", config.query);
     println!("In file: {}\n", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Apllication error: {e}");
         process::exit(1);
     }
 }
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments\n (Usage: cargo run -- text file.txt)");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
